6285ef14cfe04733d6837c4d2e4f742025495bea
Mustafa Ilikkan <milikkan@yahoo.com>
2024-03-20 20:32:06 +0300

    complete basic tic-tac-toe game

--- lesson_3/tic-tac-toe/tic-tac-toe.js
+++ lesson_3/tic-tac-toe/tic-tac-toe.js
@@ -1,20 +1,13 @@
-// 1. Display initial empty 3x3 board
-// 2. Player marks a move
-// 3. Computer marks a move
-// 4. Display updated board
-// 5. If it is a winning board, display winner
-// 6. If the board is full, display tie
-// 7. If neither player won and board it not full, go to step #2
-// 8. Ask play again?
-// 9. If yes, go to step #1
-// 10.Display goodbye
-
 const readline = require('readline-sync');
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'x';
 const COMPUTER_MARKER = 'O';
 
 function displayBoard(board) {
+  console.clear();
+
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}.`);
+
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
@@ -46,6 +39,42 @@ function emptySquares(board) {
   return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
 }
 
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+    [1, 5, 9], [3, 5, 7]             // diagonals
+  ];
+  
+  for (let line = 0; line < winningLines.length; line++) {
+    let [sq1, sq2, sq3] = winningLines[line];
+
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      return 'Player';
+    } else if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
+}
+
+function someoneWon(board) {
+  return !!detectWinner(board);
+}
+
 function playerChoosesSquare(board) {
   let square;
   
@@ -67,10 +96,33 @@ function computerChoosesSquare(board) {
   board[square] = COMPUTER_MARKER;
 }
 
-let board = initializeBoard();
-displayBoard(board);
+while (true) {
+  let board = initializeBoard();
+  displayBoard(board);
 
-playerChoosesSquare(board);
-computerChoosesSquare(board);
+  while (true) {
+    displayBoard(board);
 
-displayBoard(board);
+    playerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+    
+    computerChoosesSquare(board);
+    displayBoard(board);
+
+    if (someoneWon(board) || boardFull(board)) break;
+  }
+
+  displayBoard(board);
+
+  if (someoneWon(board)) {
+    prompt(`${detectWinner(board)} won!`);
+  } else {
+    prompt("It's a tie!");
+  }
+
+  prompt('Would you like to play again? y/n');
+  let answer = readline.question().toLowerCase()[0];
+  if (answer !== 'y') break;
+}
+
+prompt('Thanks for playing Tic Tac Toe!');
